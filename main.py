b964077b443a588f83e288756ec39b29429f55db
Sayali Patil <sayalirpatil02@gmail.com>
2026-06-11 10:39:05 +0530

    Ddone

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# import streamlit as st
-# import joblib
-# import numpy as np
-
-# # Load trained model
-# model = joblib.load("regression_model.joblib")
-
-# # App title
-# st.title("Package Prediction App")
-# st.write("Enter your CGPA to predict the expected package.")
-
-# # User input
-# cgpa = st.number_input(
-#     "Enter CGPA",
-#     min_value=0.0,
-#     max_value=10.0,
-#     value=7.0,
-#     step=0.1
-# )
-
-# # Predict button
-# if st.button("Predict Package"):
-#     input_data = np.array([[cgpa]])
-
-#     prediction = model.predict(input_data)
-
-#     st.success(f"Predicted Package: {prediction[0]:.2f} LPA")
-
 import streamlit as st
 import joblib
 import numpy as np
